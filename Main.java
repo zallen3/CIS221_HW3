64eb32beaeb807fff923256572b8fe33f2cbd8e6
Zachery Allen <allenz3794@gmail.com>
2022-02-08 15:40:59 -0500

    Initial Commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+/* 
+Zachery Allen
+CIS221 
+T & Th 2:10pm 
+*/
+
+//Create Main Class
+public class Main{
+	//main() method to setName() of MyCat, and print it through getName()
+	public static void main(String[] args){
+		MyCat myObj = new MyCat();
+		myObj.setName("Marti, the irritating");
+		System.out.println(myObj.getName());
+	}
+}
---
+++ MyCat.java
@@ -0,0 +1,20 @@
+/* 
+Zachery Allen
+CIS221 
+T & Th 2:10pm 
+*/
+
+//Create MyCat class
+public class MyCat {
+	//Private string to be changed through the getter and setter
+	private String name;
+	//getter for name
+	public String getName(){
+		return name;
+
+	}
+	//setter for name
+	public void setName(String newName){
+		this.name = newName;
+	}
+}
